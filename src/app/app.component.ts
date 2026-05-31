fc0e8e7c0ef6261b6d4184cd1bbd88ae096f55dc
GabMGallegos <gabriel25molina@hotmail.com>
2026-05-30 22:16:45 -0500

    feat: configure angular base structure and global styles

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+
+@Component({
+    selector: 'app-root',
+    imports: [
+        RouterOutlet,
+        NavbarComponent,
+        FooterComponent,
+        SpinnerComponent
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+})
+export class AppComponent {}
